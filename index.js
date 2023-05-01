912f735daaa6ed68d43530b7cec1bca6ff7e2e7c
joynalabedin05 <joynal05101993@gmail.com>
2023-05-01 22:31:49 +0600

    recipe json file added

--- index.js
+++ index.js
@@ -4,9 +4,15 @@ const cors = require('cors');
 app.use(cors());
 const port = process.env.PORT || 5000;
 
+const recipe = require('./recipe.json');
+
 app.get('/', (req, res)=>{
     res.send('Dragon is running')
 });
+
+app.get('/recipe',(req, res)=>{
+    res.send(recipe);
+})
 
 app.listen(port, ()=>{
     console.log(`dragon is running on ${port}`);
